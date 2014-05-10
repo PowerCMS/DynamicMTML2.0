d94995e8472213c5bf22ae0fe32a3066e4876e38
Junnama Noda <junnama@alfasado.jp>
2014-05-10 14:50:27 +0900

    Fixed. $app->log Faild in SQLServer.

--- addons/DynamicMTML.pack/php/class.mt_log.php
+++ addons/DynamicMTML.pack/php/class.mt_log.php
@@ -1,7 +1,42 @@
 <?php
-require_once( "class.baseobject.php" );
+require_once( 'class.baseobject.php' );
 class Log extends BaseObject {
     public $_table = 'mt_log';
     protected $_prefix = "log_";
+    public function Save() {
+        $mt = MT::get_instance();
+        if ( $created_on = $this->created_on ) {
+            $this->created_on = $mt->db()->ts2db( $created_on );
+        }
+        if ( $modified_on = $this->modified_on ) {
+            $this->modified_on = $mt->db()->ts2db( $modified_on );
+        }
+        $driver = $mt->config( 'ObjectDriver' );
+        if ( strpos( $driver, 'SQLServer' ) !== FALSE ) {
+            // UMSSQLServer
+            $sql = 'select max(log_id) from mt_log';
+            $res = $mt->db()->Execute( $sql );
+            $max = $res->_array[ 0 ][ '' ];
+            $sql = 'SET IDENTITY_INSERT mt_log ON';
+            $mt->db()->Execute( $sql );
+            $max++;
+            $this->id = $max;
+            try {
+                $this->Insert();
+            } catch ( Exception $e ) {
+                // $e->getMessage();
+                $max += 10;
+                $this->id = $max;
+                try {
+                    $res = $this->Insert();
+                } catch ( Exception $e ) {
+                }
+            }
+            $sql = 'SET IDENTITY_INSERT mt_log OFF';
+            $mt->db()->Execute( $sql );
+        } else {
+            $res = parent::Save();
+        }
+    }
 }
 ?>
